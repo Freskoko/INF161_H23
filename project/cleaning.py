38ddfb54a34bcf519f01182af7a368c99aea5a91
henrikubuntu <frishcoco@gmail.com>
2023-09-09 23:50:18 +0200

    working kind of in the middle of stuff trying big change

--- project/cleaning.py
+++ project/cleaning.py
@@ -1,42 +1,100 @@
-#TODO make a dataframe which can be used in ML model
-#TODO Data splitting, description, visualisation, and feature engineering
+# TODO make a dataframe which can be used in ML model
+# TODO Data splitting, description, visualisation, and feature engineering
+# TODO make src and utils formatting
+# TODO miniconda or env to easy work
 
 import os
-import pandas as pd
-from pathlib import Path
 from datetime import datetime
-import re
 from io import StringIO
+from pathlib import Path
+import numpy as np
 
+import pandas as pd
+
+# get current filepath to use when opening/saving files
 PWD = Path().absolute()
 
-def treat_florida_files(filename):
-    df = pd.read_csv(filename,delimiter=",")
 
-    #this time is in CEST
+def treat_florida_files(filename):
+    df = pd.read_csv(filename, delimiter=",")
+
+    # this time is in CEST
     df["DateFormatted"] = df.apply(
-        lambda row: datetime.strptime(row["Dato"] + row["Tid"], '%Y-%m-%d%H:%M'), axis=1)
+        lambda row: datetime.strptime(row["Dato"] + row["Tid"], "%Y-%m-%d%H:%M"), axis=1
+    )
+
+    #TODO LOOK AT THIS -> only in 2022 and 2023
+    # error = ignore so files without this col wont throw KeyError
+    df = df.drop(columns=["Relativ luftfuktighet"], errors="ignore")
+
+    #lets combine the 6 values for an hour period into new rows containg the mean of the values
+    
+    #drop these cols to avoid trying to get the mean's of strings
+    df = df.drop(columns=['Dato', 'Tid'])  # drop 'Dato' and 'Tid' columns
+    
+    df.set_index('DateFormatted', inplace=True)
+
+    df = df.resample('H').mean()
+
+    print(df)
 
     return df
 
+
 def treat_trafikk_files(filename):
 
-    #open file
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         my_csv_text = f.read()
 
-    #replace | with ; to get uniform delimiter, and open to StringIO to be read by od
-    csvStringIO = StringIO(my_csv_text.replace("|",";"))
+    # replace | with ; to get uniform delimiter, and open to StringIO to be read by pandas
+    csvStringIO = StringIO(my_csv_text.replace("|", ";"))
 
-    df = pd.read_csv(csvStringIO,delimiter=";",header=None) 
+    df = pd.read_csv(csvStringIO, delimiter=";")
+
+
+    # this time is in UTC +02:00, example format -> 2015-07-16T15:00+02:00
+    # we wish to transform it to the same time format as in the florida files
+    df["DateFormatted"] = df.apply(
+        lambda row: datetime.strptime(row["Fra"], "%Y-%m-%dT%H:%M%z").strftime(
+            "%Y-%m-%d %H:%M:%S"
+        ),
+        axis=1,
+    )
+
+    #certain cols are missing values and are "-", this makes it diffiuclt to work with
+    #converting to NaN helps
+    df['Trafikkmengde'] = df['Trafikkmengde'].replace('-', np.nan).astype(float)
+
+    #TODO
+    #pivot the dataframe? 
+    #basically i want there to only need to be one value of dateformatted,
+    #not 4 values like now, because 4 values makes the data hard to line up with the previous data
+
+    # Pivot the table
+    pivot_df = pd.pivot_table(df, values='Trafikkmengde', index='DateFormatted', columns='Felt')
+    # pivot_df.fillna(value, inplace=True)
+    print(pivot_df)
+
+        # save to have a look at the data
+        
+    #---------------------------
 
     directory = f"{str(PWD)}/out"
-    df.to_csv(f"{directory}/check.csv") # :warning: when saving, delimiter becomes comma
+    pivot_df.to_csv(
+        f"{directory}/check_pivot.csv")
 
-   
-    #this time is in 2015-07-16T15:00+02:00 so UTC +02:00
-    return
+      # :warning: when saving, delimiter becomes comma
+    #---------------------------
 
+    return pivot_df
+
+
+
+
+    # we care about totalt i retning danmarksplass, and florida
+    #
+
+    
 
 
 def main():
@@ -46,17 +104,26 @@ def main():
 
     for filename in os.scandir(directory):
 
-        if "Florida fefw " in str(filename):
+        if "Florida" in str(filename):
             florida_df = treat_florida_files(f"{str(directory)}/{filename.name}")
             florida_df_list.append(florida_df)
-        
+
         if "trafikkdata" in str(filename):
             trafikk_df = treat_trafikk_files(f"{str(directory)}/{filename.name}")
 
-    big_florida_df = pd.concat(florida_df_list, axis = 0)
-    combined = pd.concat([big_florida_df,trafikk_df], axis = 1)
-    return combined
-        
+    big_florida_df = pd.concat(florida_df_list, axis=0)
+
+    df_final = pd.merge(
+        big_florida_df, trafikk_df, how="inner", left_index=True, right_index=True
+    )
+
+    return df_final
+
+
 if __name__ == "__main__":
-    main()
-    # print(main())
+    # out = main()
+    main_df = (main())
+
+    directory = f"{str(PWD)}/out"
+    main_df.to_csv(
+        f"{directory}/check_main.csv")
--- project/datadocs.md
+++ project/datadocs.md
@@ -3,3 +3,23 @@
 ## Trafikkdata
 
 timestamp
+
+figurd out
+timestamp has two seperrators, replace aLL | WITH ;
+make timestamp unfiorm to make a good df
+
+# Issues
+
+## With parsing
+
+Some florida files have "Relativ luftfuktighet" some dont!
+Is this important?? -> yes i think so but this data only exists
+in 2022 and 2023, and the rest of the data sets misses ut
+
+## With the data
+
+Weather data has 5 data points for an hour
+Traffic data only has 1!
+
+Solution? -> take the mean?
+df_weather_resampled = df_weather.resample('H').mean() # or median()
